da50fc7ebe173e870bd7b553d477414662039d3e
Nicole Osborn <nicole.y.osborn@gmail.com>
2020-06-05 10:13:03 -0700

    Toggle is working!

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,14 +1,21 @@
 import useLocalStorage from './useLocalStorage'
 
 const useDarkMode =(key, initialValue)=>{
-    const [value, setValue] = useLocalStorage(false);
+    const [value, setValue] = useLocalStorage(key);
 
-    const toggleDarkMode = value =>{
+    const toggleMode = e => {
+        e.preventDefault();
+        setValue(!value);
+        console.log(value)
         if (value === true){
-            document.body.classList.add(dark-mode);
+            document.body.classList.add('dark-mode');
         } else{
-            document.body.classList.remove(dark-mode);
-        }
+            document.body.classList.remove('dark-mode');
+      };
+      
+        
     }
+    return [toggleMode, value];
 }
 export default useDarkMode;
+
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,24 +1,20 @@
 import { useState } from 'react';
-// set up state property
-// capture the values
-// if local data use that, else use initial data
-// update localStorage when needed
-// Don't forget to parse data from localStorage, and stringify new data getting stored
+
 export const useLocalStorage = (key, initialValue) => {
     const [storedValue, setStoredValue] = useState(() => {
-        // Get from local storage by key
         const item = window.localStorage.getItem(key);
-        // Parse and return stored json or, if undefined, return initialValue
         return item ? JSON.parse(item) : initialValue;
       });
+      const setValue = value => {
+        // Save state
+        setStoredValue(value);
+        // Save to local storage
+        window.localStorage.setItem(key, JSON.stringify(value));
+      };
 
-  return [storedValue];
+  return [storedValue, setValue];
 };
-const setValue = value => {
-    // Save state
-    setStoredValue(value);
-    // Save to local storage
-    window.localStorage.setItem(key, JSON.stringify(value));
-  };
+
 // should work almost exactly like useState - except any value that is controlled by this hook will be saved and persisted to localStorage
 export default useLocalStorage;
+
